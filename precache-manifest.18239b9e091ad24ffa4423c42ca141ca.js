e9931ed4f26c2249da2307b87a709a30218de6e7
Your name here <your_email@example.com>
2020-09-12 23:26:36 -0700

    Updates

---
+++ precache-manifest.18239b9e091ad24ffa4423c42ca141ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b9fd479ee4534b46ef154feea71a7d8",
+    "url": "/keyword_app/index.html"
+  },
+  {
+    "revision": "6a55d9863cb5accb6520",
+    "url": "/keyword_app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "e5c924d463ba76d251ef",
+    "url": "/keyword_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a55d9863cb5accb6520",
+    "url": "/keyword_app/static/js/2.41a184e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keyword_app/static/js/2.41a184e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c924d463ba76d251ef",
+    "url": "/keyword_app/static/js/main.ab32d340.chunk.js"
+  },
+  {
+    "revision": "691f29d6eeb46456c228",
+    "url": "/keyword_app/static/js/runtime-main.acff215c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keyword_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
